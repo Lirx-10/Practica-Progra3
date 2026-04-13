0c9a5b7013fc0214b43697dfef66a5fc6ed6eb43
Lirx-10 <leonel.reynaga@alu.ing.unlp.edu.ar>
2026-04-13 00:44:22 -0300

    13/4

--- src/Practica3/Ejercicio1/GeneralTree.java
+++ src/Practica3/Ejercicio1/GeneralTree.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class GeneralTree<T> {
     private T data;
-    private List<GeneralTree<T>> children;
+    private List<GeneralTree<T>> children = new LinkedList<GeneralTree<T>>();
     
     public GeneralTree(){}
 
@@ -12,11 +12,59 @@ public class GeneralTree<T> {
         this.data = data;
     }
     public GeneralTree(T data, List<GeneralTree<T>> hijos){
-        this.data = data;
+        this(data);
         this.children = hijos;
     }
 
     public void setData(T data){
         this.data = data;
     }
+    public T getData(){
+        return this.data;
+    }
+
+    public void setChildren(List<GeneralTree<T>> wachines){
+        if(children != null){
+            this.children = wachines;
+        }
+    }
+    public List<GeneralTree<T>> getChildren(){
+        return this.children;
+    }
+
+    public void addChild(GeneralTree<T> wachin){
+        this.children.add(wachin);
+    }
+    public void removeChild(GeneralTree<T> child) {
+		if (this.hasChildren()) {
+			List<GeneralTree<T>> children = this.getChildren();
+			if (children.contains(child))
+				children.remove(child);
+		}
+	}
+
+    public boolean hasChildren(){
+        if((!this.children.isEmpty()) & (this.children != null)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isEmpty(){
+        if(this.data == null & hasChildren()==false){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isLeaf(){
+        if(hasChildren()==false){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
---
+++ src/Practica3/Ejercicio2/RecorridosAG.java
@@ -0,0 +1,14 @@
+package src.Practica3.Ejercicio2;
+
+import java.util.List;
+
+import src.Practica3.Ejercicio1.GeneralTree;
+
+public class RecorridosAG {
+    
+    public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree <Integer> a,Integer n){
+        
+        return null;
+    }
+
+}
